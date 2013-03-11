484b51a17379d95aea5ef19b3da9fb7d023c6993
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-03-11 04:50:56 +0200

    Refactoring

--- src/main/java/Demo7.java
+++ src/main/java/Demo7.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
@@ -15,6 +17,8 @@ import com.lagodiuk.decisiontree.Predicate;
 import com.lagodiuk.decisiontree.RandomForest;
 
 public class Demo7 {
+
+	private static final Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	private static final int TRAINING_SET_SIZE = 1000;
 
@@ -31,30 +35,111 @@ public class Demo7 {
 	}
 
 	public static void main(String[] args) throws Exception {
-		List<Item> trainingSet = trainingSet();
+		List<Item> trainingSet = createTrainingSet();
+		log.info("Training set created");
 
 		DecisionTreeBuilder tbuilder =
 				DecisionTree
 						.createBuilder()
 						.setTrainingSet(trainingSet)
-						.setDefaultPredicates(Predicate.GTE, Predicate.LTE)
-						.setMinimalNumberOfItems(1);
+						.setDefaultPredicates(Predicate.GTE, Predicate.LTE);
 
 		DecisionTree tree = tbuilder.createDecisionTree();
+		log.info("Decision tree created");
 
 		RandomForest forest = RandomForest.create(tbuilder, 7);
+		log.info("Random forest created");
 
-		BufferedImage bi = new BufferedImage((WIDTH * 3) + 2, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		displayResults(args, trainingSet, tree, forest);
+	}
+
+	private static List<Item> createTrainingSet() {
+		List<Item> items = new ArrayList<Item>(TRAINING_SET_SIZE);
+
+		for (int i = 0; i < TRAINING_SET_SIZE; i++) {
+			int x = random.nextInt(WIDTH);
+			int y = random.nextInt(HEIGHT);
+
+			Item item = newItem(x, y);
+
+			if (isRed(x, y)) {
+				item.setCategory(Type.RED);
+			} else {
+				item.setCategory(Type.GREEN);
+			}
+
+			items.add(item);
+		}
+		return items;
+	}
+
+	private static Item newItem(int x, int y) {
+		return new Item()
+				.setAttribute("x", x)
+				.setAttribute("y", y);
+	}
+
+	private static boolean isRed(int x, int y) {
+		x = x - (WIDTH / 2);
+		y = y - (HEIGHT / 2);
+		return ((x * x) + (y * y)) <= (RADIUS * RADIUS);
+	}
+
+	private static void displayResults(String[] args, List<Item> trainingSet, DecisionTree tree, RandomForest forest) throws IOException {
+		BufferedImage bi = createBufferedImage();
+
 		displayTrainingSet(trainingSet, bi);
+
 		displayDecisionTree(tree, bi);
+
 		displayForest(forest, bi);
-		ImageIO.write(bi, "png", new File("./img/test.png"));
+
+		saveToFile(args, bi);
+	}
+
+	private static BufferedImage createBufferedImage() {
+		return new BufferedImage((WIDTH * 3) + 2, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	}
+
+	private static void displayTrainingSet(List<Item> trainingSet, BufferedImage bi) {
+		for (Item item : trainingSet) {
+			int x = (Integer) item.getFieldValue("x");
+			int y = (Integer) item.getFieldValue("y");
+
+			switch (((Type) item.getCategory())) {
+				case RED:
+					bi.setRGB(x, y, Color.RED.getRGB());
+					break;
+				case GREEN:
+					bi.setRGB(x, y, Color.GREEN.getRGB());
+					break;
+			}
+		}
+	}
+
+	private static void displayDecisionTree(DecisionTree tree, BufferedImage bi) {
+		for (int x = 0; x < WIDTH; x++) {
+			for (int y = 0; y < HEIGHT; y++) {
+
+				Item item = newItem(x, y);
+
+				switch ((Type) tree.classify(item)) {
+					case RED:
+						bi.setRGB(x + WIDTH + 1, y, Color.RED.getRGB());
+						break;
+					case GREEN:
+						bi.setRGB(x + WIDTH + 1, y, Color.GREEN.getRGB());
+						break;
+				}
+			}
+		}
 	}
 
 	private static void displayForest(RandomForest forest, BufferedImage bi) {
 		for (int x = 0; x < WIDTH; x++) {
 			for (int y = 0; y < HEIGHT; y++) {
-				Item item = item(transformX(x), transformY(y));
+
+				Item item = newItem(x, y);
 
 				Map<Object, Integer> result = forest.classify(item);
 
@@ -69,96 +154,19 @@ public class Demo7 {
 					}
 				}
 
-				bi.setRGB(x + (WIDTH * 2) + 2, y, new Color(((float) red) / (red + green), ((float) green) / (red + green), 0).getRGB());
+				float fRed = ((float) red) / (red + green);
+				float fGreen = ((float) green) / (red + green);
+
+				bi.setRGB(x + (WIDTH * 2) + 2, y, new Color(fRed, fGreen, 0).getRGB());
 			}
 		}
 	}
 
-	private static void displayDecisionTree(DecisionTree tree, BufferedImage bi) {
-		for (int x = 0; x < WIDTH; x++) {
-			for (int y = 0; y < HEIGHT; y++) {
-				Item item = item(transformX(x), transformY(y));
-
-				switch ((Type) tree.classify(item)) {
-					case RED:
-						bi.setRGB(x + WIDTH + 1, y, Color.RED.getRGB());
-						break;
-					case GREEN:
-						bi.setRGB(x + WIDTH + 1, y, Color.GREEN.getRGB());
-						break;
-				}
-			}
+	private static void saveToFile(String[] args, BufferedImage bi) throws IOException {
+		String outPath = "./img/test.png";
+		if (args.length > 0) {
+			outPath = args[0];
 		}
+		ImageIO.write(bi, "png", new File(outPath));
 	}
-
-	private static void displayTrainingSet(List<Item> trainingSet, BufferedImage bi) {
-		for (Item item : trainingSet) {
-			int x = (Integer) item.getFieldValue("x");
-			int y = (Integer) item.getFieldValue("y");
-
-			x = antiTransformX(x);
-			y = antiTransformY(y);
-
-			switch (((Type) item.getCategory())) {
-				case RED:
-					bi.setRGB(x, y, Color.RED.getRGB());
-					break;
-				case GREEN:
-					bi.setRGB(x, y, Color.GREEN.getRGB());
-					break;
-			}
-		}
-	}
-
-	private static List<Item> trainingSet() {
-		List<Item> items = new ArrayList<Item>(TRAINING_SET_SIZE);
-		for (int i = 0; i < TRAINING_SET_SIZE; i++) {
-
-			int x = random.nextInt(WIDTH);
-			int y = random.nextInt(HEIGHT);
-
-			x = transformX(x);
-			y = transformX(y);
-
-			Item item = item(x, y);
-
-			if (isRed(x, y)) {
-				item.setCategory(Type.RED);
-			} else {
-				item.setCategory(Type.GREEN);
-			}
-
-			items.add(item);
-		}
-		return items;
-	}
-
-	private static boolean isRed(int x, int y) {
-		// return (random.nextDouble() < 0.9) && (((x * x) + (y * y)) <= (RADIUS
-		// * RADIUS));
-		return ((x * x) + (y * y)) <= (RADIUS * RADIUS);
-	}
-
-	private static int transformX(int x) {
-		return x - (WIDTH / 2);
-	}
-
-	private static int transformY(int y) {
-		return y - (HEIGHT / 2);
-	}
-
-	private static int antiTransformX(int x) {
-		return x + (WIDTH / 2);
-	}
-
-	private static int antiTransformY(int y) {
-		return y + (HEIGHT / 2);
-	}
-
-	private static Item item(int x, int y) {
-		return new Item()
-				.setAttribute("x", x)
-				.setAttribute("y", y);
-	}
-
 }
